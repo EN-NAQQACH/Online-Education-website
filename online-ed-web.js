4131a24939ae66fa6fd71385beef2a55b3af667e
EN-NAQQACH MOHSSINE <ENNAQQACH@users.noreply.github.com>
2023-02-01 18:35:25 +0100

    last update

--- online-ed-web.js
+++ online-ed-web.js
@@ -6,3 +6,74 @@ menubtn.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+/* --Courses Section-- */
+
+var swiper = new Swiper(".course-slider", {
+    spaceBetween:20,
+    /*grabCursor:true,*/
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+ 
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
+
+  /* --Teachers Section-- */
+
+var swiper = new Swiper(".teachers-slide", {
+    spaceBetween:20,
+    /*grabCursor:true,*/
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+ 
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
+
+    /* --Reviews Section-- */
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween:20,
+    /*grabCursor:true,*/
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+ 
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
